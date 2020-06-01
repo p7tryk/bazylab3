105529362a84f6a8f6632504b15578d7b1c34be1
thisconnect <p7tryk@gmail.com>
2020-06-01 17:25:50 +0200

    refactor kilku funkcji w interface

--- src/bazylab3/Interface.java
+++ src/bazylab3/Interface.java
@@ -5,44 +5,62 @@ import java.awt.BorderLayout;
 import javax.swing.*;
 
 public class Interface
-{
-	//tutaj bedzie interfejs generowany i zmieniany
-	//TODO wszystko?
-		
-	/*
-	 * tak ogolnie mysle zeby tutaj byl array tablic = Mysql.getTables
-	 * i do kazdej tabeli byla array kolumn = Mysql.getColumns
-	 * wtedy od razu bedzie wiadomo jak rozlozyci interfejs w sensie miejsca
-	 */
-	JFrame okno;
-	Mysql db1;
-	public Interface()
 	{
-		//
-		stworzOkno();
-		test();
+		// tutaj bedzie interfejs generowany i zmieniany
+		// TODO wszystko?
+
+		/*
+		 * tak ogolnie mysle zeby tutaj byl array tablic = Mysql.getTables i do kazdej
+		 * tabeli byla array kolumn = Mysql.getColumns wtedy od razu bedzie wiadomo jak
+		 * rozlozyci interfejs w sensie miejsca
+		 */
+		JFrame okno;
+		Mysql db1;
+		Permissions user1;
+
+		public Interface()
+			{
+				stworzOkno();
+				login("admin", "pwsz", "localhost", 3306);
+				test();
+				okno.setVisible(true);
+				logout();
+			}
+
+		public void stworzOkno()
+			{
+				okno = new JFrame("Baza Filmow");
+				okno.setSize(1024, 768);
+				okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			}
+
+		public void login(String user, String password, String ip, int port)
+			{
+				user1 = new Permissions(user, password);
+				db1 = new Mysql(user1, ip, port);
+			}
+
+		public void logout()
+			{
+				db1.close();
+				System.out.print("logging out\n");
+			}
+
+		public void drawTable(String data[][], String header[])
+			{
+				System.out.print("drawing table\n");
+				// To w sumie wydaje sie banalne
+				JTable tabela = new JTable(data, header);
+
+				okno.setLayout(new BorderLayout());
+				okno.add(tabela.getTableHeader(), BorderLayout.PAGE_START);
+				okno.add(tabela, BorderLayout.CENTER);
+			}
+
+		public void test()
+			{
+				String[][] output = db1.select("select * from filmy");
+				String[] kolumny = db1.getColumns("select * from filmy");
+				drawTable(output, kolumny);
+			}
 	}
-	public void stworzOkno()
-	{
-		okno = new JFrame("Baza Filmow");
-		okno.setSize(800,600);
-	}
-	public void test()
-	{
-		String output[][];
-		String kolumny[];
-		Permissions user1 = new Permissions("admin","pwsz");
-		db1 =  new Mysql(user1,"127.0.0.1",3306);
-		output = db1.select("select * from filmy");
-		kolumny = db1.getColumns("select * from filmy");
-		
-		//To w sumie wydaje sie banalne
-		JTable tabela = new JTable(output, kolumny);
-		
-		okno.setLayout(new BorderLayout());
-		okno.add(tabela.getTableHeader(), BorderLayout.PAGE_START);
-		okno.add(tabela, BorderLayout.CENTER);
-		okno.setVisible(true);
-		db1.close();
-	}
-}
